7609fcb3d891d2ddaf24e402c3501ad2473eafc2
demo <mhdmubassir11@gamil.com>
2025-11-26 23:58:30 +0530

    navbar minimize done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,32 +4,41 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <>
-        <nav className="nav-wrapper">
-            <div className="nav-content">
-                <img className="logo" src="" alt=""/>
+            <nav className="nav-wrapper">
+                <div className="nav-content">
+                    <img className="logo" src="" alt="" />
 
-                <ul>
-                    <li>
-                        <a className="menu-item">Home</a>
-                    </li>
-                    <li>
-                        <a className="menu-item">Skills</a>
-                    </li>
-                    <li>
-                        <a className="menu-item">Work Experience</a>
-                    </li>
-                    <li>
-                        <a className="menu-item">Contact Me</a>
-                    </li>
-                    
-                    <button className="contact-btn" onClick={() => {}}>
-                        Hire me
-                    </button>
-                </ul>
+                    <ul>
+                        <li>
+                            <a className="menu-item">Home</a>
+                        </li>
+                        <li>
+                            <a className="menu-item">Skills</a>
+                        </li>
+                        <li>
+                            <a className="menu-item">Work Experience</a>
+                        </li>
+                        <li>
+                            <a className="menu-item">Contact Me</a>
+                        </li>
+
+                        <button className="contact-btn" onClick={() => { }}>
+                            Hire me
+                        </button>
+                    </ul>
+
+                    <button class="menu-btn" onClick={() => {}}>
+                    <span
+                        class={"meterial-symbols-outlined"}
+                        style={{ fontSize: "1.8rem" }}
+                    >
+                        menu
+                    </span>
+                </button>
             </div>
-        </nav>
+        </nav >
         </>
-    )
-}
+    );
+};
 
 export default Navbar
